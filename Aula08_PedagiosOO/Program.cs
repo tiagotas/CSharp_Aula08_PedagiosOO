b4bf297c4a1d51f83dd77b1e43355644d21bf18c
TIAGO SILVA <frost.tiago@gmail.com>
2020-10-13 12:40:38 -0300

    Outras formas de declarar uma variável.

--- Aula08_PedagiosOO/Program.cs
+++ Aula08_PedagiosOO/Program.cs
@@ -9,16 +9,18 @@ namespace Aula08_PedagiosOO
         {
             NumberFormatInfo nfi = new CultureInfo("pt-BR").NumberFormat;
 
+            Pedagio p2, p3, p4, p5, p6;
+
             /**
              * Faça um programa em que deverá ser programada uma sequencia de objetos
-             * que recebam localização e valor dos pedagios  de Mococa a São Paulo.
+             * que recebam localização e valor dos pedagios entre Mococa e São Paulo.
              * No fim da sequencia de objetos, apresente o valor total dos pedágios.
              */
             Pedagio p1 = new Pedagio();
             p1.Localizacao = "Entre Mococa e Casa Branca";
             p1.Valor = 6.10;
 
-            Pedagio p2 = new Pedagio();
+            p2 = new Pedagio();
             p2.Localizacao = "Entre Casa Branca e Aguaí";
             p2.Valor = 6.40;
 
@@ -27,19 +29,19 @@ namespace Aula08_PedagiosOO
              * Definindo a propriedade localização com uma string
              * Definindo a propriedade valor como um número decimal
              */ 
-            Pedagio p3 = new Pedagio();
+            p3 = new Pedagio();
             p3.Localizacao = "Entre Aguaí e Mogi Guaçu";
             p3.Valor = 7.10;
 
-            Pedagio p4 = new Pedagio();
+            p4 = new Pedagio();
             p4.Localizacao = "Entre Mogi Guaçu e Campinas";
             p4.Valor = 12.00;
 
-            Pedagio p5 = new Pedagio();
+            p5 = new Pedagio();
             p5.Localizacao = "Entre Campinas e Jundiaí";
             p5.Valor = 9.10;
 
-            Pedagio p6 = new Pedagio();
+            p6 = new Pedagio();
             p6.Localizacao = "Entre Jundiaí e São Paulo";
             p6.Valor = 9.20;
 
